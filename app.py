2d66450c825614dde95cf2b7a572edc66cd794e9
John Exterkamp <john.exterkamp@icloud.com>
2024-12-02 21:23:02 -0500

    default route & sources

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, redirect, url_for
 from database import init_db
 from routes import register_route_blueprints
 from views import register_view_blueprints
@@ -13,6 +13,10 @@ init_db(app)
 # Register blueprints
 register_route_blueprints(app)
 register_view_blueprints(app)
+
+@app.route("/", methods=["GET"])
+def index():
+    return redirect(url_for('kiosk.dashboard'))
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=Config.PORT, debug=Config.Production)
